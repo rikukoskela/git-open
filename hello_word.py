9f7a7262631639db5e3c9cd8c0827aed0bfe8f1d
Riku Koskela <riku.koskela@tuni.fi>
2025-05-07 12:18:41 +0300

    Create hello_word.py

---
+++ hello_word.py
@@ -0,0 +1,7 @@
+$ git clone https://github.com/rikukoskela/git-open.git
+print(Hello World!)
+$ git status
+$ nano hello_world.py
+$ git add hello_world.py
+$ git commit -m "Initial commit"
+$ git push
