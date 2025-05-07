65baa1028409a29cfe25e590dfd06e2a2b8388b0
Riku Koskela <riku.koskela@tuni.fi>
2025-05-07 16:43:05 +0300

    Delete mergesort.py

--- mergesort.py
+++
@@ -1,5 +0,0 @@
-git pull https://course-gitlab.tuni.fi/git-course/basics-materials.git
-git fetch https://course-gitlab.tuni.fi/git-course/basics-materials.git
-git merge mergesort.py--allow-unrelated-histories
-git config pull.rebase false
-
